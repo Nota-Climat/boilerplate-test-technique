74dda3f4b3562f0a8d13623e3ed1e6decbe602b5
Cyrillus <chenetcyril@free.fr>
2023-12-04 17:39:49 +0100

    add css to ClimateDataUpdateForm

--- front/src/components/ClimateDataUpdateForm/ClimateDataUpdateForm.js
+++ front/src/components/ClimateDataUpdateForm/ClimateDataUpdateForm.js
@@ -1,89 +1,96 @@
 import infoIcone from '../../infoIcone.png';
+import './ClimateDataUpdateForm.css';
 
 function ClimateDataUpdateForm() {
     return (
-        <div>
-            <div className="">
-                <div className="">
-                    <div className="">
+        <div className='ClimateDataUpdateForm'>
+            <div className='ClimateDataUpdateForm-container'>
+                <div className="ClimateDataUpdateForm-updateInfo">
+                    <div className="ClimateDataUpdateForm-updateInfo-imageContainer">
                         <img
-                            className=""
+                            className="ClimateDataUpdateForm-updateInfo-imageContainer-image"
                             src={infoIcone}
                             alt=""
                         />
                     </div>
-                    <div>
+                    <div className='ClimateDataUpdateForm-updateInfo-textContainer'>
                         <p>La mise à jour des données est un dialogue entre votre entreprise et OpenClimat qui vérifie les données. Aucune information soumise dans ce formulaire ne sera publiée sans discussion préalable.</p>
                         <p>Prochaine analyse: <strong>Décembre 2023</strong></p>
                     </div>
                 </div>
-            </div>
 
-            <div className="">
-                <h1>Vous souhaitez demander une mise à jour de votre page publique OpenClimat ?</h1>
-                <p>Voici les élements que vous pouvez mettre à jour (tout ou partie)</p>
-                <ul>
-                    <li>Performance climatique (% de réduction des émissions sur vos scopes 1&2 et 3)</li>
-                    <li>Bilan carbone (données brutes de vos émissions sur vos scopes 1&2 et 3)</li>
-                    <li>Labels liés au climat (SBTi, CDP)</li>
-                </ul>
-            </div>
+                <div className="ClimateDataUpdateForm-updateDescription">
+                    <h1>Vous souhaitez demander une mise à jour de votre page publique OpenClimat ?</h1>
+                    <p>Voici les élements que vous pouvez mettre à jour (tout ou partie)
+                        <ul>
+                            <li>Performance climatique (% de réduction des émissions sur vos scopes 1&2 et 3)</li><br/>
+                            <li>Bilan carbone (données brutes de vos émissions sur vos scopes 1&2 et 3)</li><br/>
+                            <li>Labels liés au climat (SBTi, CDP)</li><br/>
+                        </ul>
+                    </p>
+                </div>
 
-            <div className="">
-                <h1>Contact</h1>
-                <form className="">
-                    <div>
-                       <label className="">
-                            Nom
-                            <br/>
-                            <input
-                                value=""
-                                type="text"
-                                placeholder=""                        
-                        />
-                        </label>
-                        <label className="">
-                            Prénom
-                            <br/>
-                            <input
-                                value=""
-                                type="text"
-                                placeholder=""                        
+                <div className="ClimateDataUpdateForm-line"></div>
+
+                <div className="ClimateDataUpdateForm-updateForm">
+                    <h1>Contact</h1>
+                    <form className="">
+                        <div className="ClimateDataUpdateForm-updateForm-groupLabel">
+                        <label>
+                                Nom
+                                <br/>
+                                <input
+                                    value=""
+                                    type="text"
+                                    placeholder=""
                             />
-                        </label> 
-                    </div>
-                    <div>
-                        <label className="">
-                            Votre poste
-                            <br/>
-                            <input
-                                value=""
-                                type="text"
-                                placeholder=""                        
-                            />
-                        </label>
-                    </div>                    
-                    <div>
-                        <label className="">
-                            Adresse email
-                            <br/>
-                            <input
-                                value=""
-                                type="text"
-                                placeholder=""                        
-                            />
-                        </label>
-                        <label className="">
-                            Numéro de téléphone
-                            <br/>
-                            <input
-                                value=""
-                                type="text"
-                                placeholder=""                        
-                            />
-                        </label>   
-                    </div>                    
-                </form>
+                            </label>
+                            <label className="">
+                                Prénom
+                                <br/>
+                                <input
+                                    value=""
+                                    type="text"
+                                    placeholder=""
+                                />
+                            </label>
+                        </div >
+                        <div className="ClimateDataUpdateForm-updateForm-groupLabel">
+                            <label className="">
+                                Votre poste
+                                <br/>
+                                <input
+                                    value="toto"
+                                    type="text"
+                                    placeholder=""
+                                />
+                            </label>
+                        </div>
+                        <div className="ClimateDataUpdateForm-updateForm-groupLabel">
+                            <label className="">
+                                Adresse email
+                                <br/>
+                                <input
+                                    value=""
+                                    type="text"
+                                    placeholder=""
+                                />
+                            </label>
+                            <label className="">
+                                Numéro de téléphone
+                                <br/>
+                                <input
+                                    value=""
+                                    type="text"
+                                    placeholder=""
+                                />
+                            </label>
+                        </div>
+                    </form>
+                </div>
+            </div>
+            <div className='ClimateDataUpdateForm-buttonContainer'>
+                <button className='ClimateDataUpdateForm-buttonContainer-button'>Commencer</button>
             </div>
         </div>
     )
