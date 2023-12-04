aebbd5918095de4e2560656319bca6f657332291
Cyrillus <chenetcyril@free.fr>
2023-12-04 18:03:49 +0100

    add UpdateDescription component

--- front/src/components/ClimateDataUpdateForm/ClimateDataUpdateForm.js
+++ front/src/components/ClimateDataUpdateForm/ClimateDataUpdateForm.js
@@ -1,5 +1,6 @@
 import Button from '../Button/Button';
 import Line from '../Line/Line';
+import UpdateDescription from './UpdateDescription/UpdateDescription';
 import UpdateInfo from './UpdateInfo/UpdateInfo';
 import './ClimateDataUpdateForm.css';
 
@@ -8,18 +9,7 @@ function ClimateDataUpdateForm() {
         <div className='ClimateDataUpdateForm'>
             <div className='ClimateDataUpdateForm-container'>
                 <UpdateInfo/>
-
-                <div className="ClimateDataUpdateForm-updateDescription">
-                    <h1>Vous souhaitez demander une mise à jour de votre page publique OpenClimat ?</h1>
-                    <p>Voici les élements que vous pouvez mettre à jour (tout ou partie)
-                        <ul>
-                            <li>Performance climatique (% de réduction des émissions sur vos scopes 1&2 et 3)</li><br/>
-                            <li>Bilan carbone (données brutes de vos émissions sur vos scopes 1&2 et 3)</li><br/>
-                            <li>Labels liés au climat (SBTi, CDP)</li><br/>
-                        </ul>
-                    </p>
-                </div>
-
+                <UpdateDescription/>
                 <Line className={"ClimateDataUpdateForm-line"} />
 
                 <div className="ClimateDataUpdateForm-updateForm">
---
+++ front/src/components/ClimateDataUpdateForm/UpdateDescription/UpdateDescription.js
@@ -0,0 +1,18 @@
+import './UpdateDescription.css'
+
+function UpdateDescription() {
+    return (
+        <div className="UpdateDescription">
+            <h1>Vous souhaitez demander une mise à jour de votre page publique OpenClimat ?</h1>
+            <p>Voici les élements que vous pouvez mettre à jour (tout ou partie)
+                <ul>
+                    <li>Performance climatique (% de réduction des émissions sur vos scopes 1&2 et 3)</li><br/>
+                    <li>Bilan carbone (données brutes de vos émissions sur vos scopes 1&2 et 3)</li><br/>
+                    <li>Labels liés au climat (SBTi, CDP)</li><br/>
+                </ul>
+            </p>
+        </div>
+    )
+}
+
+export default UpdateDescription;
